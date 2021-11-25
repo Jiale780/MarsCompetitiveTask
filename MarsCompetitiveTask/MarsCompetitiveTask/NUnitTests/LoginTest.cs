fded67bb42846ba0d5c3ee6b77b0b506f31cfff8
JL_Yeoh <37150682+WOD56Tech0@users.noreply.github.com>
2021-11-26 01:08:00 +0800

    Added of the login page and home page files

---
+++ MarsCompetitiveTask/MarsCompetitiveTask/NUnitTests/LoginTest.cs
@@ -0,0 +1,25 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Model;
+using MarsCompetitiveTask.Pages;
+using MarsCompetitiveTask.Utils;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace MarsCompetitiveTask.NUnitTests
+{
+    [TestFixture]
+    class LoginTest : CommonDriver
+    {
+        [Test]
+        public void LoginPageTest()
+        {
+            test = extent.CreateTest("Login");
+            test.Log(Status.Info, "User Login");
+
+            // Login Page object initialization and definition
+            LoginPage loginPageObj = new LoginPage(testDriver);
+            loginPageObj.LoginSteps(testDriver);
+            TestContext.WriteLine(loginPageObj);
+        }
+    }
+}
